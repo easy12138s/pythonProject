0294d33c11706d14226ba72acff091cb88e0a7d8
easy <2418087868@qq.com>
2023-09-14 20:56:04 +0800

    20230914-home

--- 网络爬虫/金融股票/可转债数据/可转债信息.py
+++ 网络爬虫/金融股票/可转债数据/可转债信息.py
@@ -12,8 +12,13 @@ headers = {
     'Referer': 'https://data.eastmoney.com/'
 }
 # 获取数据
-# url = 'https://datacenter-web.eastmoney.com/api/data/v1/get?callback=jQuery112305445752770008432_1694676002292&sortColumns=PUBLIC_START_DATE&sortTypes=-1&pageSize=50&pageNumber=1&reportName=RPT_BOND_CB_LIST&columns=ALL&quoteColumns=f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE%2Cf235~10~SECURITY_CODE~TRANSFER_PRICE%2Cf236~10~SECURITY_CODE~TRANSFER_VALUE%2Cf2~10~SECURITY_CODE~CURRENT_BOND_PRICE%2Cf237~10~SECURITY_CODE~TRANSFER_PREMIUM_RATIO%2Cf239~10~SECURITY_CODE~RESALE_TRIG_PRICE%2Cf240~10~SECURITY_CODE~REDEEM_TRIG_PRICE%2Cf23~01~CONVERT_STOCK_CODE~PBV_RATIO&quoteType=0&source=WEB&client=WEB'
-#
+url = ('https://datacenter-web.eastmoney.com/api/data/v1/get?callback=jQuery112305445752770008432_1694676002292'
+       '&sortColumns=PUBLIC_START_DATE&sortTypes=-1&pageSize=50&pageNumber=1&reportName=RPT_BOND_CB_LIST&columns=ALL'
+       '&quoteColumns=f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE%2Cf235~10~SECURITY_CODE~TRANSFER_PRICE%2Cf236~10'
+       '~SECURITY_CODE~TRANSFER_VALUE%2Cf2~10~SECURITY_CODE~CURRENT_BOND_PRICE%2Cf237~10~SECURITY_CODE'
+       '~TRANSFER_PREMIUM_RATIO%2Cf239~10~SECURITY_CODE~RESALE_TRIG_PRICE%2Cf240~10~SECURITY_CODE~REDEEM_TRIG_PRICE'
+       '%2Cf23~01~CONVERT_STOCK_CODE~PBV_RATIO&quoteType=0&source=WEB&client=WEB')
+
 # resp = requests.get(url, headers=headers)
 # data = resp.text.replace('jQuery112305445752770008432_1694676002292(', '').replace(');', '')
 # data_dict = json.loads(data)["result"]["data"]
