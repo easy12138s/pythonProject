468ad0cc8df5f26f70b236ad981b5d94df9b2144
easy <2418087868@qq.com>
2023-09-20 17:20:56 +0800

    20230920-work

--- 网络爬虫/金融股票/可转债数据/可转债信息.py
+++ 网络爬虫/金融股票/可转债数据/可转债信息.py
@@ -1,33 +1,18 @@
 import json
-import requests
 import re
-import warnings
+import requests
 from bs4 import BeautifulSoup as bs, Comment
+from 请求配置信息 import dongFang
+import warnings
 warnings.filterwarnings('ignore')
 
-# 模拟浏览器请求配置信息
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.76',
-    'Referer': 'https://data.eastmoney.com/'
-}
-# 获取数据
-url = ('https://datacenter-web.eastmoney.com/api/data/v1/get?callback=jQuery112305445752770008432_1694676002292'
-       '&sortColumns=PUBLIC_START_DATE&sortTypes=-1&pageSize=50&pageNumber=1&reportName=RPT_BOND_CB_LIST&columns=ALL'
-       '&quoteColumns=f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE%2Cf235~10~SECURITY_CODE~TRANSFER_PRICE%2Cf236~10'
-       '~SECURITY_CODE~TRANSFER_VALUE%2Cf2~10~SECURITY_CODE~CURRENT_BOND_PRICE%2Cf237~10~SECURITY_CODE'
-       '~TRANSFER_PREMIUM_RATIO%2Cf239~10~SECURITY_CODE~RESALE_TRIG_PRICE%2Cf240~10~SECURITY_CODE~REDEEM_TRIG_PRICE'
-       '%2Cf23~01~CONVERT_STOCK_CODE~PBV_RATIO&quoteType=0&source=WEB&client=WEB')
-
-# resp = requests.get(url, headers=headers)
-# data = resp.text.replace('jQuery112305445752770008432_1694676002292(', '').replace(');', '')
-# data_dict = json.loads(data)["result"]["data"]
-# print(data_dict)
+resp_data, resp_field = dongFang()
 
 # 获取字段信息
-# url = 'https://data.eastmoney.com/xg/xg/?mkt=kzz'
-# resp = requests.get(url, headers=headers)
-# soup = bs(resp.text, 'html.parser')
-# data = soup.find("div", {"id": "dataview_kzz"}).find_all("th")
-# data_sou = re.findall(r'[\u4e00-\u9fa5]', data)
-# print(data_sou)
+soup = bs(resp_field.text, 'html.parser')
+print(soup.text)
+
+# 可转债数据
+data = resp_data.text.replace('jQuery112306648817856021285_1695195221752(', '').replace(');', '')
+data_dict = json.loads(data)["result"]["data"]
+
---
+++ 网络爬虫/金融股票/可转债数据/请求配置信息.py
@@ -0,0 +1,53 @@
+"""
+    爬虫请求头配置信息
+"""
+import requests
+
+# 东方财富可转债
+def dongFang():
+    cookies = {
+        'qgqp_b_id': 'e943d73977c5642211ae816d6df7f2ef',
+        'st_si': '20123219818259',
+        'st_asi': 'delete',
+        'JSESSIONID': '6EEA5A47B18477D75D3E7D5A7BEAB3C9',
+        'st_pvi': '56500704703528',
+        'st_sp': '2022-12-06%2015%3A57%3A11',
+        'st_inirUrl': 'https%3A%2F%2Fwww.baidu.com%2Flink',
+        'st_sn': '6',
+        'st_psi': '20230920153058675-113300300986-2060725315',
+    }
+
+    headers = {
+        'Accept': '*/*',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Connection': 'keep-alive',
+        # 'Cookie': 'qgqp_b_id=e943d73977c5642211ae816d6df7f2ef; st_si=20123219818259; st_asi=delete; JSESSIONID=6EEA5A47B18477D75D3E7D5A7BEAB3C9; st_pvi=56500704703528; st_sp=2022-12-06%2015%3A57%3A11; st_inirUrl=https%3A%2F%2Fwww.baidu.com%2Flink; st_sn=6; st_psi=20230920153058675-113300300986-2060725315',
+        'Referer': 'https://data.eastmoney.com/xg/xg/?mkt=kzz',
+        'Sec-Fetch-Dest': 'script',
+        'Sec-Fetch-Mode': 'no-cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+    }
+
+    params = {
+        'callback': 'jQuery112306648817856021285_1695195221752',
+        'sortColumns': 'PUBLIC_START_DATE',
+        'sortTypes': '-1',
+        'pageSize': '10',
+        'pageNumber': '1',
+        'reportName': 'RPT_BOND_CB_LIST',
+        'columns': 'ALL',
+        'quoteColumns': 'f2~01~CONVERT_STOCK_CODE~CONVERT_STOCK_PRICE,f235~10~SECURITY_CODE~TRANSFER_PRICE,f236~10~SECURITY_CODE~TRANSFER_VALUE,f2~10~SECURITY_CODE~CURRENT_BOND_PRICE,f237~10~SECURITY_CODE~TRANSFER_PREMIUM_RATIO,f239~10~SECURITY_CODE~RESALE_TRIG_PRICE,f240~10~SECURITY_CODE~REDEEM_TRIG_PRICE,f23~01~CONVERT_STOCK_CODE~PBV_RATIO',
+        'quoteType': '0',
+        'source': 'WEB',
+        'client': 'WEB',
+    }
+    url_field = 'https://data.eastmoney.com/xg/xg/?mkt=kzz'
+    url_data = 'https://datacenter-web.eastmoney.com/api/data/v1/get'
+    response_data = requests.get(url_data, params=params, cookies=cookies, headers=headers)
+    response_field = requests.get(url_field, params=params, cookies=cookies, headers=headers)
+
+    return response_data, response_field
