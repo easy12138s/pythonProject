d967e0f19500540b9bb9015fa26c28778c8b4874
easy <2418087868@qq.com>
2023-10-20 10:53:25 +0800

    20231020-work

--- 网络爬虫/test.py
+++ 网络爬虫/test.py
@@ -8,38 +8,36 @@
     @time: 2023/10/19 17:15
 """
 import json
-
 import requests
+cookies = {
+    'SINAGLOBAL': '115445762559.89148.1689583983224',
+    'UOR': ',,login.sina.com.cn',
+    'ULV': '1696819297273:7:1:1:8610694135791.372.1696819297270:1695880142641',
+    'XSRF-TOKEN': 'Luj5hSiSEzoABcJkSIiCQqQS',
+    'SUB': '_2AkMSbWd2f8NxqwFRmPAWzmjkboR0zwjEieKkMZatJRMxHRl-yT9kqnxctRB6Oe1JmZabPK7dAvgeriICelP2-rabFk6x',
+    'SUBP': '0033WrSXqPxfM72-Ws9jqgMF55529P9D9Wh1uo6l4lX7LYJSG1YJ_DSB',
+    'WBPSESS': 'eyDLPcU90tHVPRrsWrtAPvJJMu2507deQZmPTsltY-Q7xX8osLamqSwWS6xfm97GQlNeS32Cvm9NfUollja60WH5jDIrtOtdtFP3q-NdGQJIKe3s3BYm9iglI_p5PWUjdskEXS7L4we2Ad9frs9g-jga9TRY8sBgaTnJVJbIYiw=',
+}
 
 headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVpZCI6IjY0YmEyZDk4OTVkNzIxNjVlNjRhYzkwOSIsInZlcnNpb24iOjAsImJyYW5jaCI6InpoIn0sImlhdCI6MTY5NzY5ODUyNCwiZXhwIjoxNjk3ODcxMzI0fQ.eB9cYq9Z5sXki8_iT7Lqc1x0ADcRJUMBeZ8ClsuISjo',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/json;charset=UTF-8',
-    'Origin': 'chrome-extension://aikflfpejipbpjdlfabpgclhblkpaafo',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'none',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-    'i-app': 'hitab',
-    'i-branch': 'zh',
-    'i-lang': 'zh-CN',
-    'i-platform': 'chrome',
-    'i-version': '1.3.6',
+    'authority': 'weibo.com',
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'client-version': 'v2.44.3',
+    # 'cookie': 'SINAGLOBAL=115445762559.89148.1689583983224; UOR=,,login.sina.com.cn; ULV=1696819297273:7:1:1:8610694135791.372.1696819297270:1695880142641; XSRF-TOKEN=Luj5hSiSEzoABcJkSIiCQqQS; SUB=_2AkMSbWd2f8NxqwFRmPAWzmjkboR0zwjEieKkMZatJRMxHRl-yT9kqnxctRB6Oe1JmZabPK7dAvgeriICelP2-rabFk6x; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9Wh1uo6l4lX7LYJSG1YJ_DSB; WBPSESS=eyDLPcU90tHVPRrsWrtAPvJJMu2507deQZmPTsltY-Q7xX8osLamqSwWS6xfm97GQlNeS32Cvm9NfUollja60WH5jDIrtOtdtFP3q-NdGQJIKe3s3BYm9iglI_p5PWUjdskEXS7L4we2Ad9frs9g-jga9TRY8sBgaTnJVJbIYiw=',
+    'referer': 'https://weibo.com/newlogin?tabtype=search&gid=&openLoginLayer=0&url=https%3A%2F%2Fweibo.com%2F',
     'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'server-version': 'v2023.10.19.2',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+    'x-xsrf-token': 'Luj5hSiSEzoABcJkSIiCQqQS',
 }
 
-json_data = {
-    'prompt': '你好',
-    'assistantId': '',
-    'model': '650e52e9c4bcb4a52791b599',
-}
-
-response = requests.post('https://wetabchat.haohuola.com/api/chat/conversation-v2', headers=headers, json=json_data)
-date = response.text
-
-date_list = list(date.split('_e79218965e_'))
-print(date_list)
+response = requests.get('https://weibo.com/ajax/side/hotSearch', cookies=cookies, headers=headers)
+data = json.loads(response.text)
+print(data['data'])
