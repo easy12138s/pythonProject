a4a59f5e5a6dee0b554231e43c210e6847a5295d
easy <2418087868@qq.com>
2023-11-07 16:43:50 +0800

    20231107-work

---
+++ 数据分析/Numpy/demo4.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+"""
+    @version: python3.1
+    @author:'EASY'
+    @contact: 2418087868@qq.com
+    @software: PyCharm
+    @file: demo4.py
+    @time: 2023/11/7 13:56
+"""
+import numpy as np
