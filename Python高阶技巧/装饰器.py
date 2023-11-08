3c0f0e8138c6685ae0ddcef55b5bce13995656a3
easy <2418087868@qq.com>
2023-11-08 15:29:59 +0800

    20231108-work

--- Python高阶技巧/装饰器.py
+++ Python高阶技巧/装饰器.py
@@ -1,8 +1,12 @@
 """
-    装饰器也是一种闭包，
-    功能是为了不破坏目标函数的前提下，为目标函数新增功能
-"""
+    装饰器也是一种闭包，功能是为了不破坏目标函数的前提下，为目标函数新增功能
+    ①不修改已有函数的源代码
+    ②不修改已有函数的调用方式
+    ③给已有函数增加额外的功能
 
+    装饰器函数通过接收一个函数参数传递给装饰器函数内部的嵌套函数，最后返回内部函数，其中内部函数大致分为原函数调用，调用前和调用后
+
+"""
 
 def outer(func):
     def inner():
@@ -20,3 +24,44 @@ def sleep():
     time.sleep(random.randint(1, 5))
 
 sleep()
+
+
+# 装饰带参数的函数或者是带返回值的函数，装饰器都要有相关体现，调用运用了装饰器的函数sum_num()其实就是调用装饰器函数inner()
+def logging(fn):
+    def inner(num1, num2):
+        print('--正在努力计算--')
+        fn(num1, num2)
+    return inner
+
+@logging
+def sum_num(num1, num2):
+    result = num1 + num2
+    print(result)
+
+sum_num(10, 20)
+
+"""
+    类装饰器的使用：通过定义一个类来装饰函数。
+    
+    这段代码定义了一个装饰器类 Check，用于在被装饰的函数执行前进行一些操作，
+    例如检查用户是否已经登录。 在类的初始化方法 init 中，将被装饰的函数 fn 保存在实例属性 fn 中。
+    
+    在类中定义了一个特殊方法 call，该方法会在类实例像函数一样被调用时执行。在 call 方法中，先输出一条提示信息，然后调用保存在 fn 属性中的函数。
+    
+    
+"""
+class Check(object):
+    def __init__(self, fn):
+        # 初始化操作在此完成
+        self.__fn = fn
+
+    # 实现__call__方法，表示把类像调用函数一样进行调用。
+    def __call__(self, *args, **kwargs):
+        # 添加装饰功能
+        print("请先登陆...")
+        self.__fn()
+@Check
+def comment():
+    print("发表评论")
+
+comment()
--- Python高阶技巧/闭包.py
+++ Python高阶技巧/闭包.py
@@ -1,19 +1,26 @@
 """
-    闭包特性示例
+    作用域就是变量的作用范围，在哪里可以使用，在哪里不可以使用。
+    Python中变量随着函数的出现，作用域可以划分两种情况：① 全局作用域 ② 局部作用域
+    随着作用域的出现，变量也被强制划分两种类型：① 全局变量 ② 局部变量
+    ① 全局变量：在全局作用域中定义的变量就是全局变量
+    ② 局部变量：在局部作用域中定义的变量就是局部变量
 
+    闭包特性示例
     1.外部函数中定义了一个内部函数
     2.外部函数总是返回内部函数的引用
     3.内部函数可以使用外部函数提供的环境变量
+
+    闭包可以保存外部函数内的变量，不会随着外部函数调用完而销毁。 注意点: 由于闭包引用了外部函数的变量，则外部函数的变量没有及时释放，消耗内存。
 """
 
 
 # 简单闭包
 def outer(logo):
-
     def inner(msg):
         print(f"<{logo}>{msg}<{logo}>")
 
     return inner
+
 
 fn = outer("easy")
 fn("hi!")
@@ -26,7 +33,6 @@ fn2("hi!")
 # nonlocal 修改外部函数值
 # ATM机案例
 def account_create(initial_amount=0):
-
     def atm(num, deposit=True):
         nonlocal initial_amount
         if deposit:
@@ -38,7 +44,25 @@ def account_create(initial_amount=0):
 
     return atm
 
+
 atm = account_create()
 atm(100)
 atm(100)
 atm(50, deposit=False)
+
+
+def outer():
+    result = 0
+
+    def inner(num):
+        nonlocal result
+        result += num
+        print(result)
+
+    return inner
+
+
+f = outer()
+f(1)
+f(2)
+f(3)  # 问这次的执行结果是多少？
