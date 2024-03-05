8a897e0dfe4d8b17893bac05e2005746e2c2a72e
easy_wu <2418087868@qq.com>
2024-03-05 14:57:40 +0800

    2024-03-05（work）

--- 网络爬虫/test.py
+++ 网络爬虫/test.py
@@ -1,15 +1,14 @@
-import pandas as pd
-import numpy as np
+import json
+from urllib.request import urlopen
+
+import requests
 
 
-weather_df = pd.read_csv('E:\pythonProject\网络爬虫\JS逆向\gamer520\games.csv')
+def getCountry(ipAddress):
+    response = requests.get("https://freegeoip.net/json/" + ipAddress).read().decode('utf-8')
+    print(response)
+    responseJson = json.loads(response)
+    return responseJson.get("country_code")
 
-weather_df['三级下载地址'] = (weather_df['三级下载地址'].str.replace('\'', '').str.replace(': ', ':')
-              .str.replace('点我  ', '').str.replace(' [立即下载]', '').str.replace('[', '').str.replace(']', '')
-              .str.split('  '))
 
-data_list = weather_df['三级下载地址'].values.tolist()
-weather_df['三级下载地址'] = data_list[0][0]
-weather_df['提取码'] = data_list[0][1]
-weather_df['解压密码'] = data_list[0][-1]
-weather_df.to_csv('test.csv')
+print(getCountry("50.78.253.58"))
